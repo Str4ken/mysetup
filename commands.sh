7d5311d525ab85b1fdf37305b2d51a9740e2079f
Str4ken <github@robinschnider.com>
2018-05-03 12:45:07 +0200

    added commands.sh

---
+++ commands.sh
@@ -0,0 +1,7 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew tap caskroom/cask
+brew tap drone/drone
+brew install cask drone
+
+brew cask install 1clipboard github libreoffice slack sublime-text visual-studio-code docker google-chrome pycharm-ce spotify virtualbox
