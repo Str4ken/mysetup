480b463ae79d5cfb75de2b803514ee443cb346e0
Str4ken <github@robinschnider.com>
2018-05-05 10:38:52 +0200

    update apps

--- commands.sh
+++ commands.sh
@@ -2,6 +2,27 @@
 
 brew tap caskroom/cask
 brew tap drone/drone
-brew install cask drone
 
-brew cask install 1clipboard github libreoffice slack sublime-text visual-studio-code docker google-chrome pycharm-ce spotify virtualbox
+brew install cask drone git pipenv python postgresql
+
+brew cask install   1clipboard
+                    battle-net
+                    discord
+                    docker
+                    github
+                    google-backup-and-sync
+                    google-chrome
+                    google-hangouts
+                    google-web-designer
+                    jdownloader
+                    league-of-legends
+                    libreoffice
+                    papers
+                    pycharm-ce
+                    slack
+                    spotify
+                    steam
+                    sublime-text
+                    thunderbird
+                    virtualbox
+                    visual-studio-code
